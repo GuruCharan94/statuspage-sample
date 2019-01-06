d00973548f78605ad3f159970fe2c648d776c3e8
Gurucharan Subramani <guru11294@gmail.com>
2019-01-06 17:14:21 +0000

    Initial Commit Statusfy Project

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "Sample Title",
+  "name": "sample_title",
+  "description": "Sample Desc",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "json"
+  }
+}
